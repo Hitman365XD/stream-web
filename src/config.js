22cc8713285c45f505f582d8cc1a7bfea0bd65ce
Renzo_Tacuri <renzito480@gmail.com>
2026-07-29 15:31:00 -0500

    variables de entorno para vercel

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+const env = import.meta.env;
+
 export default {
-    streamUrl: "http://streaming.local/hls/stream.m3u8",
-    twitchParent: "streaming.local",
-    response: "http://streaming.local:3000/stream-data"
-}
+  streamUrl: env.VITE_STREAM_URL,
+  twitchParent: env.VITE_TWITCH_PARENT,
+  response: env.VITE_RESPONSE_URL,
+};
