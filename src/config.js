7a64de3fba59bd570d0bef8e4375b9bfa1219265
Renzo480 <renzito480@gmail.com>
2026-08-09 16:01:17 -0500

    refactor: Cambio de nombres en variables

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const env = import.meta.env;
 
 export default {
-  streamUrl: env.VITE_STREAM_URL,
+  streamUrl: env.VITE_VIDEO_URL,
   twitchParent: env.VITE_TWITCH_PARENT,
-  response: env.VITE_RESPONSE_URL,
+  response: env.VITE_DATA_URL,
 };
